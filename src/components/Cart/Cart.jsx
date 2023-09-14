8644f6c066779035aa12f102c8b5391cc030a3c1
ASRAF JAHAN <webasraf3@gmail.com>
2023-09-14 18:43:07 +0600

    project done

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,18 @@
+import './Cart.css'
+const Cart = ({selectedActors,remaining,totalCost}) => {
+    console.log(selectedActors)
+    return (
+        <div>
+            <h3>Total actors:{selectedActors.length}</h3>
+            <h5>Remaining:{remaining}</h5>
+            <h5>TotalCost:{totalCost}</h5>
+            {
+                selectedActors.map(actor => (
+                    <li key={actor.id}>{actor.name}</li>
+                ))
+            }
+        </div>
+    );
+};
+
+export default Cart;
